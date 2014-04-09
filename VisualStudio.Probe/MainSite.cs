0f142891f685c83650eaabc895fc3974710a7d66
Francesco Pretto <ceztko@gmail.com>
2014-04-10 01:27:47 +0200

    Enhanced the probe template wizard to return a list of projects
    Also add an example of use of the generated project from the
    template.

--- VisualStudio.Probe/MainSite.cs
+++ VisualStudio.Probe/MainSite.cs
@@ -22,9 +22,15 @@ using DTEProject = EnvDTE.Project;
 using System.IO;
 using Microsoft.VisualStudio.VCProjectEngine;
 using Microsoft.VisualStudio.Modeling.Shell;
+using VisualStudio.Support;
+using Microsoft.VisualStudio.Project.Framework.INTERNAL.VS2010ONLY;
+using Microsoft.VisualStudio.Project.Contracts.INTERNAL.VS2010ONLY;
 
 namespace VisualStudio.Probe
 {
+    //extern alias VC;
+    //using VC::Microsoft.VisualStudio.Project.VisualC.VCProjectEngine;
+
     [PackageRegistration(UseManagedResourcesOnly = true)]
     [InstalledProductRegistration("#110", "#112", "1.0", IconResourceID = 400)]
     [Guid(GuidList.VSPackageGuidString)]
@@ -48,12 +54,68 @@ namespace VisualStudio.Probe
 
         void _DTEEvents_OnStartupComplete()
         {
+            DoWork();
+        }
+
+        private void DoWork()
+        {
+            ProjectCollection.GlobalProjectCollection.ProjectAdded += new ProjectCollection.ProjectAddedEventHandler(GlobalProjectCollection_ProjectAdded);
+            ProjectCollection.GlobalProjectCollection.ProjectChanged += new EventHandler<ProjectChangedEventArgs>(GlobalProjectCollection_ProjectChanged);
+            ProjectCollection.GlobalProjectCollection.ProjectCollectionChanged += new EventHandler<ProjectCollectionChangedEventArgs>(GlobalProjectCollection_ProjectCollectionChanged);
+            ProjectCollection.GlobalProjectCollection.ProjectXmlChanged += new EventHandler<ProjectXmlChangedEventArgs>(GlobalProjectCollection_ProjectXmlChanged);
+
+            Directory.Delete(@"C:\Temp", true);
+            Directory.CreateDirectory(@"C:\Temp");
+
             Solution2 solution = (Solution2)_DTE2.Solution;
             // create a new solution
             solution.Create(@"C:\Temp\", "NewSolution");
-            solution.AddFromTemplate(@"C:\Users\ceztko\Documents\GitHub\SideBuilder\Resources\TemplatesVS10\Win32Dll\MyTemplate.vstemplate", @"C:\Temp\Test", "Test");
-            solution.AddFromTemplate(@"C:\Users\ceztko\Documents\GitHub\SideBuilder\Resources\TemplatesVS10\Win32Dll\MyTemplate.vstemplate", @"C:\Temp\Test2", "Test2");
+
+            List<DTEProject> projects = ProbeTemplate.AddFromTemplate(_DTE2, @"C:\Users\ceztko\Documents\GitHub\SideBuilder\Resources\TemplatesVS10\Win32Dll\MyTemplate.vstemplate", @"C:\Temp\Test", "Test");
+            //DTEProject project2 = solution.AddFromTemplate(@"C:\Users\ceztko\Documents\GitHub\SideBuilder\Resources\TemplatesVS10\Win32Dll\MyTemplate.vstemplate", @"C:\Temp\Test2", "Test2");
+
             solution.SaveAs(@"C:\Temp\NewSolution.sln");
+
+            DTEProject project = projects.FirstOrDefault();
+            VCProject vcproject = project.Object as VCProject;
+
+            IVCCollection configurations = vcproject.Configurations;
+
+            VCConfiguration configuration = configurations.Item("Debug");
+
+            ConfiguredProject confproject = configuration.GetConfiguredProject();
+            IProjectPropertiesProvider propProvider = confproject.GetProjectPropertiesProvider();
+            propProvider.ProjectPropertyChanged += new EventHandler<ProjectPropertyChangedEventArgs>(propProvider_ProjectPropertyChanged);
+
+            configuration.ConfigurationType = ConfigurationTypes.typeStaticLibrary;
+
+            //solution.AddFromTemplate(@"C:\Users\ceztko\Documents\GitHub\SideBuilder\Resources\TemplatesVS10\Win32Dll\MyTemplate.vstemplate", @"C:\Temp\Test2", "Test2");
+
+        }
+
+        void propProvider_ProjectPropertyChanged(object sender, ProjectPropertyChangedEventArgs e)
+        {
+
+        }
+
+        void GlobalProjectCollection_ProjectXmlChanged(object sender, ProjectXmlChangedEventArgs e)
+        {
+
+        }
+
+        void GlobalProjectCollection_ProjectCollectionChanged(object sender, ProjectCollectionChangedEventArgs e)
+        {
+
+        }
+
+        void GlobalProjectCollection_ProjectChanged(object sender, ProjectChangedEventArgs e)
+        {
+
+        }
+
+        void GlobalProjectCollection_ProjectAdded(object sender, ProjectCollection.ProjectAddedToProjectCollectionEventArgs e)
+        {
+
         }
 
         private void GetService<T>(out T service)
---
+++ VisualStudio.Probe/ProbeTemplate.cs
@@ -0,0 +1,119 @@
+﻿// Copyright (c) 2014 Francesco Pretto
+// This file is subject to the MIT license
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TemplateWizard;
+using EnvDTE;
+using EnvDTE80;
+using System.Threading;
+
+namespace VisualStudio.Probe
+{
+    class ProbeTemplate : IWizard
+    {
+        private static object _Lock;
+        private static TemplateHandle _Current;
+
+        private List<Project> _Projects;
+
+        static ProbeTemplate()
+        {
+            _Lock = new object();
+            _Current = null;
+        }
+
+        public ProbeTemplate()
+        {
+            _Projects = new List<Project>();
+
+            TemplateHandle handle;
+            lock (_Lock)
+            {
+                handle = _Current;
+                if (handle != null)
+                {
+                    // AddFromTemplate has been called
+                    handle.Template = this;
+                    _Current = null;
+                }
+            }
+
+            if (handle != null)
+            {
+                // Release the first lock set on AddFromTemplate
+                Monitor.Exit(_Lock);
+            }
+        }
+
+        public static List<Project> AddFromTemplate(DTE2 dte, string filename, string destination, string projectname)
+        {
+            TemplateHandle current = new TemplateHandle();
+            Monitor.Enter(_Lock);
+            try
+            {
+                Solution2 solution = dte.Solution as Solution2;
+                _Current = current;
+                solution.AddFromTemplate(filename, destination, projectname);
+                if (current.Template != null)
+                    return current.Template.Projects;
+                
+            }
+            finally
+            {
+                _Current = null;
+                if (current.Template == null)
+                {
+                    // The lock wasn't released elsewhere
+                    Monitor.Exit(_Lock);
+                }
+            }
+
+            return null;
+        }
+
+        public void BeforeOpeningFile(ProjectItem projectItem)
+        {
+        }
+
+        public void ProjectFinishedGenerating(Project project)
+        {
+            _Projects.Add(project);
+        }
+
+        public void ProjectItemFinishedGenerating(ProjectItem projectItem)
+        {
+        }
+
+        public void RunFinished()
+        {
+        }
+
+        public void RunStarted(object automationObject, Dictionary<string, string> replacementsDictionary, WizardRunKind runKind, object[] customParams)
+        {
+            string safeprojectname = replacementsDictionary["$safeprojectname$"];
+            replacementsDictionary["$safeprjnameupcase$"] = safeprojectname.ToUpper();
+        }
+
+        public bool ShouldAddProjectItem(string filePath)
+        {
+            return true;
+        }
+
+        public List<Project> Projects
+        {
+            get { return _Projects; }
+        }
+    }
+
+    internal class TemplateHandle
+    {
+        public ProbeTemplate Template
+        {
+            get;
+            set;
+        }
+    }
+}
