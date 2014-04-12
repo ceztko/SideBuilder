c48c6007b255c40acfe3ce8b20e6adda0f5cb834
Francesco Pretto <ceztko@gmail.com>
2014-04-12 16:07:01 +0200

    Convert the probe to a test suite using NUnit

--- VisualStudio.Probe/MainSite.cs
+++ VisualStudio.Probe/MainSite.cs
@@ -15,22 +15,13 @@ using Microsoft.VisualStudio.OLE.Interop;
 using Microsoft.VisualStudio.Shell;
 using EnvDTE;
 using EnvDTE80;
-using Microsoft.Build.Evaluation;
-using System.Reflection;
-using BuildProject = Microsoft.Build.Evaluation.Project;
-using DTEProject = EnvDTE.Project;
-using System.IO;
-using Microsoft.VisualStudio.VCProjectEngine;
 using Microsoft.VisualStudio.Modeling.Shell;
-using VisualStudio.Support;
-using Microsoft.VisualStudio.Project.Framework.INTERNAL.VS2010ONLY;
-using Microsoft.VisualStudio.Project.Contracts.INTERNAL.VS2010ONLY;
+using System.Reflection;
+using NUnit.Core;
+using NUnit.Util;
 
 namespace VisualStudio.Probe
 {
-    //extern alias VC;
-    //using VC::Microsoft.VisualStudio.Project.VisualC.VCProjectEngine;
-
     [PackageRegistration(UseManagedResourcesOnly = true)]
     [InstalledProductRegistration("#110", "#112", "1.0", IconResourceID = 400)]
     [Guid(GuidList.VSPackageGuidString)]
@@ -38,10 +29,14 @@ namespace VisualStudio.Probe
     [ProvideBindingPath]
     public sealed partial class MainSite : Package
     {
-        private static DTE2 _DTE2;
-        private static DTEEvents _DTEEvents;
+        private static MainSite _Instance;
+        private DTE2 _DTE2;
+        private DTEEvents _DTEEvents;
 
-        public MainSite() { }
+        public MainSite()
+        {
+            _Instance = this;
+        }
 
         protected override void Initialize()
         {
@@ -54,83 +49,36 @@ namespace VisualStudio.Probe
 
         void _DTEEvents_OnStartupComplete()
         {
-            DoWork();
+            CoreExtensions.Host.InitializeService();
+            SimpleTestRunner remoteTestRunner = new SimpleTestRunner();
+            TestPackage package = new TestPackage(@"Probe");
+            string loc = Assembly.GetExecutingAssembly().Location;
+            package.Assemblies.Add(loc);
+            remoteTestRunner.Load(package);
+            TestResult testResult = remoteTestRunner.Run(new NullListener(), TestFilter.Empty, false, LoggingThreshold.Off);
+            XmlResultWriter writer = new XmlResultWriter(@"C:\Temp\TestResult.xml");
+            writer.SaveTestResult(testResult);
+            _DTE2.Quit();
         }
 
-        private void DoWork()
-        {
-            ProjectCollection.GlobalProjectCollection.ProjectAdded += new ProjectCollection.ProjectAddedEventHandler(GlobalProjectCollection_ProjectAdded);
-            ProjectCollection.GlobalProjectCollection.ProjectChanged += new EventHandler<ProjectChangedEventArgs>(GlobalProjectCollection_ProjectChanged);
-            ProjectCollection.GlobalProjectCollection.ProjectCollectionChanged += new EventHandler<ProjectCollectionChangedEventArgs>(GlobalProjectCollection_ProjectCollectionChanged);
-            ProjectCollection.GlobalProjectCollection.ProjectXmlChanged += new EventHandler<ProjectXmlChangedEventArgs>(GlobalProjectCollection_ProjectXmlChanged);
-
-            Directory.Delete(@"C:\Temp", true);
-            Directory.CreateDirectory(@"C:\Temp");
-
-            Solution2 solution = (Solution2)_DTE2.Solution;
-            // create a new solution
-            solution.Create(@"C:\Temp\", "NewSolution");
-
-            List<DTEProject> projects = ProbeTemplate.AddFromTemplate(_DTE2, @"C:\Users\ceztko\Documents\GitHub\SideBuilder\Resources\TemplatesVS10\Win32Dll\MyTemplate.vstemplate", @"C:\Temp\Test", "Test");
-            //DTEProject project2 = solution.AddFromTemplate(@"C:\Users\ceztko\Documents\GitHub\SideBuilder\Resources\TemplatesVS10\Win32Dll\MyTemplate.vstemplate", @"C:\Temp\Test2", "Test2");
-
-            solution.SaveAs(@"C:\Temp\NewSolution.sln");
-
-            DTEProject project = projects.FirstOrDefault();
-            VCProject vcproject = project.Object as VCProject;
-
-            IVCCollection configurations = vcproject.Configurations;
-
-            VCConfiguration configuration = configurations.Item("Debug");
-
-            ConfiguredProject confproject = configuration.GetConfiguredProject();
-            IProjectPropertiesProvider propProvider = confproject.GetProjectPropertiesProvider();
-            propProvider.ProjectPropertyChanged += new EventHandler<ProjectPropertyChangedEventArgs>(propProvider_ProjectPropertyChanged);
-
-            configuration.ConfigurationType = ConfigurationTypes.typeStaticLibrary;
-
-            //solution.AddFromTemplate(@"C:\Users\ceztko\Documents\GitHub\SideBuilder\Resources\TemplatesVS10\Win32Dll\MyTemplate.vstemplate", @"C:\Temp\Test2", "Test2");
-
-        }
-
-        void propProvider_ProjectPropertyChanged(object sender, ProjectPropertyChangedEventArgs e)
-        {
-
-        }
-
-        void GlobalProjectCollection_ProjectXmlChanged(object sender, ProjectXmlChangedEventArgs e)
-        {
-
-        }
-
-        void GlobalProjectCollection_ProjectCollectionChanged(object sender, ProjectCollectionChangedEventArgs e)
-        {
-
-        }
-
-        void GlobalProjectCollection_ProjectChanged(object sender, ProjectChangedEventArgs e)
-        {
-
-        }
-
-        void GlobalProjectCollection_ProjectAdded(object sender, ProjectCollection.ProjectAddedToProjectCollectionEventArgs e)
-        {
-
-        }
-
-        private void GetService<T>(out T service)
+        public void GetService<T>(out T service)
         {
             service = (T)GetService(typeof(T));
         }
 
-        private T GetService<T>()
+        public T GetService<T>()
         {
             return (T)GetService(typeof(T));
         }
 
-        public static DTE2 DTE2
+        public DTE2 DTE2
         {
             get { return _DTE2; }
+        }
+
+        public static MainSite Instance
+        {
+            get { return _Instance; }
         }
     }
 }
---
+++ VisualStudio.Probe/ProbeSuite.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using EnvDTE;
+using EnvDTE80;
+using Microsoft.Build.Evaluation;
+using System.Reflection;
+using BuildProject = Microsoft.Build.Evaluation.Project;
+using DTEProject = EnvDTE.Project;
+using System.IO;
+using Microsoft.VisualStudio.VCProjectEngine;
+using VisualStudio.Support;
+using Microsoft.VisualStudio.Project.Framework.INTERNAL.VS2010ONLY;
+using Microsoft.VisualStudio.Project.Contracts.INTERNAL.VS2010ONLY;
+using NUnit.Core;
+using NUnit.Framework;
+
+namespace VisualStudio.Probe
+{
+    //extern alias VC;
+    //using VC::Microsoft.VisualStudio.Project.VisualC.VCProjectEngine;
+
+    [TestFixture]
+    public class ProbeSuite
+    {
+        private MainSite _MainSite;
+        private DTE2 _DTE2;
+        private DTEProject _Project;
+
+        [TestFixtureSetUp]
+        public void Setup()
+        {
+            _MainSite = MainSite.Instance;
+            _DTE2 = _MainSite.DTE2;
+
+            ProjectCollection.GlobalProjectCollection.ProjectAdded += new ProjectCollection.ProjectAddedEventHandler(GlobalProjectCollection_ProjectAdded);
+            ProjectCollection.GlobalProjectCollection.ProjectChanged += new EventHandler<ProjectChangedEventArgs>(GlobalProjectCollection_ProjectChanged);
+            ProjectCollection.GlobalProjectCollection.ProjectCollectionChanged += new EventHandler<ProjectCollectionChangedEventArgs>(GlobalProjectCollection_ProjectCollectionChanged);
+            ProjectCollection.GlobalProjectCollection.ProjectXmlChanged += new EventHandler<ProjectXmlChangedEventArgs>(GlobalProjectCollection_ProjectXmlChanged);
+
+            Directory.Delete(@"C:\Temp", true);
+            Directory.CreateDirectory(@"C:\Temp");
+
+            Solution2 solution = (Solution2)_DTE2.Solution;
+
+            // Create a new solution
+            solution.Create(@"C:\Temp\", "NewSolution");
+
+            // Create the project
+            List<DTEProject> projects = ProbeTemplate.AddFromTemplate(_DTE2, @"C:\Users\ceztko\Documents\GitHub\SideBuilder\Resources\TemplatesVS10\Win32Dll\MyTemplate.vstemplate", @"C:\Temp\Test", "Test");
+            solution.SaveAs(@"C:\Temp\NewSolution.sln");
+
+            _Project = projects.FirstOrDefault();
+        }
+
+        [Test]
+        public void DoWork()
+        {
+            VCProject vcproject = _Project.Object as VCProject;
+            IVCCollection configurations = vcproject.Configurations;
+            VCConfiguration configuration = configurations.Item("Debug");
+
+            ConfiguredProject confproject = configuration.GetConfiguredProject();
+            IProjectPropertiesProvider propProvider = confproject.GetProjectPropertiesProvider();
+            propProvider.ProjectPropertyChanged += new EventHandler<ProjectPropertyChangedEventArgs>(propProvider_ProjectPropertyChanged);
+
+            configuration.ConfigurationType = ConfigurationTypes.typeStaticLibrary;
+        }
+
+
+        void propProvider_ProjectPropertyChanged(object sender, ProjectPropertyChangedEventArgs e)
+        {
+
+        }
+
+        void GlobalProjectCollection_ProjectXmlChanged(object sender, ProjectXmlChangedEventArgs e)
+        {
+
+        }
+
+        void GlobalProjectCollection_ProjectCollectionChanged(object sender, ProjectCollectionChangedEventArgs e)
+        {
+
+        }
+
+        void GlobalProjectCollection_ProjectChanged(object sender, ProjectChangedEventArgs e)
+        {
+
+        }
+
+        void GlobalProjectCollection_ProjectAdded(object sender, ProjectCollection.ProjectAddedToProjectCollectionEventArgs e)
+        {
+
+        }
+    }
+}
