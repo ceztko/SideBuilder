cfdf26e3c9bb5220d8e28975b0255df3f2758d47
Francesco Pretto <ceztko@gmail.com>
2014-04-25 20:08:07 +0200

    Stub ProjectElement iteration with new xbuild code evaluation

--- SideBuilder.Core/Support.cs
+++ SideBuilder.Core/Support.cs
@@ -7,26 +7,32 @@ using System.Linq;
 using System.Text;
 using Microsoft.Build.Construction;
 using Microsoft.Build.Evaluation;
+using Microsoft.Build.Internal.Expressions;
 
 namespace SideBuilder.Core
 {
     public static class Extensions
     {
-        public static IEnumerable<ProjectElement> Iterate(ProjectRootElement root, ProjectCollection collection)
+        public static IEnumerable<ProjectElement> Iterate(this Project project)
+        {
+            return Iterate(project.Xml, project);
+        }
+
+        public static IEnumerable<ProjectElement> Iterate(ProjectRootElement root, Project project)
         {
             foreach (ProjectElement element in root.Children)
             {
                 ProjectImportGroupElement group = element as ProjectImportGroupElement;
                 if (group != null)
                 {
-                    foreach (ProjectElement element2 in Iterate(group, collection))
+                    foreach (ProjectElement element2 in Iterate(group, project))
                         yield return element2;
                 }
 
                 ProjectImportElement import = element as ProjectImportElement;
                 if (import != null)
                 {
-                    foreach (ProjectElement element2 in Iterate(import, collection))
+                    foreach (ProjectElement element2 in Iterate(import, project))
                         yield return element2;
                 }
 
@@ -34,14 +40,14 @@ namespace SideBuilder.Core
             }
         }
 
-        public static IEnumerable<ProjectElement> Iterate(ProjectImportGroupElement group, ProjectCollection collection)
+        public static IEnumerable<ProjectElement> Iterate(ProjectImportGroupElement group, Project project)
         {
             foreach (ProjectElement element in group.Children)
             {
                 ProjectImportElement import = element as ProjectImportElement;
                 if (import != null)
                 {
-                    foreach (ProjectElement element2 in Iterate(import, collection))
+                    foreach (ProjectElement element2 in Iterate(import, project))
                         yield return element2;
                 }
                 else
@@ -50,10 +56,11 @@ namespace SideBuilder.Core
             }
         }
 
-        public static IEnumerable<ProjectElement> Iterate(ProjectImportElement import, ProjectCollection collection)
+        public static IEnumerable<ProjectElement> Iterate(ProjectImportElement import, Project project)
         {
-            ProjectRootElement importRoot = ProjectRootElement.Open(import.Project);
-            foreach (ProjectElement element2 in Iterate(importRoot, collection))
+            string path = new ExpressionEvaluator(project, null).Evaluate(import.Project);
+            ProjectRootElement importRoot = ProjectRootElement.Open(path, project.ProjectCollection);
+            foreach (ProjectElement element2 in Iterate(importRoot, project))
                 yield return element2;
         }
     }
