8a6ba5a48399f80e1fed4472d75d439915fa83ba
Francesco Pretto <ceztko@gmail.com>
2015-01-30 17:18:44 +0100

    SideBuilder: Added stub test project

---
+++ SideBuilder.Test/OtherTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SideBuilder.Test
+{
+    class OtherTests
+    {
+        public void foo()
+        {
+            /*
+
+            Project debugProject = BuilderCore.LoadProject(@"C:\Users\ceztko\Temp\VsProbe\Test\Test.vcxproj",
+                collection, globalProperties);
+            
+            string test = "!$([System.String]::IsNullOrEmpty('$(TargetFrameworkVersion)'))";
+
+            bool test3 = new ExpressionEvaluator(project).EvaluateAsBoolean(test);
+            test = "$(TargetFrameworkVersion) == $(TargetFramork) and $(TargetFrameworkVersion) == '4.0'";
+            bool success;
+            bool? result = new ExpressionEvaluator(project).EvaluateAsBoolean(test, out success);
+
+            ExpressionList exp2 = new ExpressionParser().Parse("Exists('$(MSBuildToolsPath)\\Microsoft.WorkflowBuildExtensions.targets')", ExpressionValidationType.StrictBoolean);
+            ExpressionList exp = new ExpressionParser().Parse(test, ExpressionValidationType.StrictBoolean);
+             */
+        }
+    }
+}
---
+++ SideBuilder.Test/TreeSparseDictionaryTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using Collections.Specialized;
+
+namespace SideBuilder.Test
+{
+    [TestFixture]
+    public class TreeSparseDictionaryTest
+    {
+        private TreeSparseDictionary<string> _Dictionary;
+
+        [TestFixtureSetUp]
+        public void Setup()
+        {
+            _Dictionary = new TreeSparseDictionary<string>();
+        }
+
+        [Test]
+        public void Foo()
+        {
+            _Dictionary["Test1"] = "Test1";
+            Assert.AreEqual(_Dictionary["Test1"], "Test1");
+        }
+    }
+}
