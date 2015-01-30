2c182c3cb7769ee98a8eacb7e92d4774595b8731
Francesco Pretto <ceztko@gmail.com>
2015-01-30 17:20:53 +0100

    Rearranged samples

---
+++ Samples/Auditor.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+using Microsoft.Build.Evaluation;
+using System.IO;
+using Microsoft.Build.Execution;
+using System.Collections;
+
+namespace Samples
+{
+    public class Auditor
+    {
+        public static string TempDir
+        {
+            get { return null; }
+        }
+
+        public static void foo(ProjectCollection collection, string path = null)
+        {
+            if (path == null)
+                path = Path.Combine(TempDir, "debug.txt");
+
+            PropertyInfo property = typeof(ProjectCollection).GetProperty("EnvironmentProperties", BindingFlags.Instance | BindingFlags.NonPublic);
+            IEnumerable dictionary = property.GetValue(collection, new object[] { }) as IEnumerable;
+
+            using (var writer = new StreamWriter(path))
+            {
+                foreach (ProjectPropertyInstance prop in dictionary)
+                {
+                    writer.WriteLine(prop.Name + "\t" + prop.EvaluatedValue);
+                }
+            }
+        }
+
+        public static void foo2(Project project, string path = null)
+        {
+            if (path == null)
+                path = Path.Combine(TempDir, "variables.txt");
+
+            using (var writer = new StreamWriter(path))
+            {
+                foreach (ResolvedImport import in project.Imports)
+                    writer.WriteLine(import.ImportedProject.FullPath);
+            }
+        }
+    }
+}
--- Samples/Program.cs
+++ Samples/Program.cs
@@ -10,6 +10,10 @@ using Microsoft.Build.Evaluation;
 using System.IO;
 using Microsoft.Build.Construction;
 using SideBuilder.Core;
+using Microsoft.Build.Execution;
+using Collections.Specialized;
+using Microsoft.Build.Expressions;
+using MSBuild.Support;
 
 namespace VisualStudio.Probe
 {
@@ -25,16 +29,32 @@ namespace VisualStudio.Probe
 
         private static void Sample1()
         {
-            string tempDir = @"C:\Users\ceztko\Temp";
+            string tempDir = @"C:\Users\ceztko\Documents\Temp";
             ProjectCollection collection = ProjectCollection.GlobalProjectCollection;
+
 
             Dictionary<string, string> globalProperties = BuilderCore.GetGlobalProperties(
                 "NewSolution", "C:\\Users\\ceztko\\Temp\\VsProbe\\", "NewSolution.sln",
                 "C:\\Users\\ceztko\\Temp\\VsProbe\\NewSolution.sln", ".sln",
                 "Debug", "Win32", "1033");
 
-            Project debugProject = BuilderCore.LoadProject("C:\\Users\\ceztko\\Temp\\VsProbe\\Test\\Test.vcxproj",
+
+            Project test5 = new Project(collection);
+
+            TreeSparseDictionary<string> dictionary = new TreeSparseDictionary<string>();
+
+            foreach (ProjectProperty property in test5.Properties)
+            {
+                dictionary[property.Name] = property.EvaluatedValue;
+            }
+
+            Project debugProject = BuilderCore.LoadProject(@"C:\Users\ceztko\Temp\VsProbe\Test\Test.vcxproj",
                 collection, globalProperties);
+
+            StreamWriter writer2 = new StreamWriter(@"C:\Users\ceztko\Temp\logica.txt");
+            debugProject.SaveLogicalProject(writer2);
+
+            Test(debugProject, dictionary);
 
             HashSet<string> test = new HashSet<string>();
             foreach (ProjectItem item in debugProject.ItemsIgnoringCondition)
@@ -44,11 +64,19 @@ namespace VisualStudio.Probe
 
             BuilderCore.Process(debugProject);
 
-            using (var writer = new StreamWriter(Path.Combine(tempDir, "debug.txt")))
-            {
-                foreach (ResolvedImport import in debugProject.Imports)
-                    writer.WriteLine(import.ImportedProject.FullPath);
-            }
+            /*
+             * 1) Si filla un ProjectStatus di property (tipo sicuramente NO CLR o altre tautologie)
+             * 2) Si unrolla Eagerly il Project/ProjectRootElement, creando branch ogni volta che ci sono delle condizioni
+             * 3) Si ritorna sulle condizioni e si cerca di entrare dove non si era entrati prima, aprendo altri status.
+             * 4) Si ritorna sugli stati di 2) e si tenta di capire se quanto fatto in 3) possa aprire altre strade 
+             */
+
+            /*
+             * 1) Si filla un ProjectStatus di property (tipo sicuramente NO CLR o altre tautologie)
+             * 2) Si unrolla Eagerly il Project/ProjectRootElement, creando branch ogni volta che si deve entrare in un import
+             * condizionale. Si CHIUDE il branch quando si esce dall'import.
+             * 
+             */
 
             globalProperties = BuilderCore.GetGlobalProperties(
                 "NewSolution", "C:\\Users\\ceztko\\Temp\\VsProbe\\", "NewSolution.sln",
@@ -57,15 +85,63 @@ namespace VisualStudio.Probe
 
             Project releaseProject = BuilderCore.LoadProject("C:\\Users\\ceztko\\Temp\\VsProbe\\Test\\Test.vcxproj",
                 collection, globalProperties);
-            using (var writer = new StreamWriter(Path.Combine(tempDir, "release.txt")))
-            {
-                foreach (ResolvedImport import in releaseProject.Imports)
-                    writer.WriteLine(import.ImportedProject.FullPath);
-            }
 
             collection.SetGlobalProperty("VSIDEResolvedNonMSBuildProjectOutputs", "<VSIDEResolvedNonMSBuildProjectOutputs />");
             collection.SetGlobalProperty("CurrentSolutionConfigurationContents",
                 "<SolutionConfiguration>\n<ProjectConfiguration Project=\"{e312acae-f92e-4ce9-8ce5-a9911bba621f}\" AbsolutePath=\"C:\\Users\\ceztko\\Temp\\VsProbe\\Test\\Test.vcxproj\">Debug|Win32</ProjectConfiguration>\n</SolutionConfiguration>");
+        }
+
+        public static void Test(Project project, TreeSparseDictionary<string> dictionary)
+        {
+            List<ExpressionList> list = new List<ExpressionList>();
+            List<bool?> results = new List<bool?>();
+
+            ProjectStatus dictWrapper = new ProjectStatus(dictionary);
+
+            foreach (ProjectElement element in project.Xml.Iterate(dictWrapper))
+            {
+                foo(element, dictionary);
+
+                if (!String.IsNullOrEmpty(element.Condition))
+                {
+                    ExpressionEvaluator evaluator = new ExpressionEvaluator(dictWrapper);
+
+                    bool success2;
+                    bool? value = evaluator.EvaluateAsBoolean(element.Condition, out success2);
+                    results.Add(value);
+
+                    ExpressionList exps = new ExpressionParser().Parse(element.Condition, ExpressionValidationType.StrictBoolean);
+                    list.Add(exps);
+                }
+            }
+        }
+
+        public static void foo(ProjectElement element, TreeSparseDictionary<string> dictionary)
+        {
+            switch (element.GetElementType())
+            {
+
+                case ElementType.ItemGroupElement:
+                {
+                    ProjectItemGroupElement itemGroup = element as ProjectItemGroupElement;
+                    break;
+                }
+                case ElementType.PropertyGroupElement:
+                {
+                    ProjectPropertyGroupElement propertyGroup = element as ProjectPropertyGroupElement;
+                    foreach (ProjectPropertyElement property in propertyGroup.Properties)
+                    {
+                        dictionary[property.Name] = property.Value;
+                    }
+                    break;
+                }
+                case ElementType.ItemDefinitionGroupElement:
+                {
+                    ProjectItemDefinitionGroupElement itemDefinitionGroup = element as ProjectItemDefinitionGroupElement;
+
+                    break;
+                }
+            }
         }
 
         private static void Sample2()
